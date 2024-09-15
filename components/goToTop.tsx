78b3739299ff5937253db4d3572e1c2833aaedfd
BishalPatgiri <bishalpatgiri67@gmail.com>
2024-09-15 23:59:43 +0530

    Added: goToUp

--- components/goToTop.tsx
+++ components/goToTop.tsx
@@ -1,11 +1,14 @@
 // GoTop.js
 import React from 'react';
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const GoTop = (props:any) => {
   return (
     <div className={props.showGoTop} onClick={props.scrollUp}>
-      <button className="fixed bottom-8 right-8 border-0 w-16 h-16 rounded-full drop-shadow-md bg-indigo-500 text-white text-3xl font-bold  hover:bg-indigo-600">
-        <i className="goTop__text fas fa-chevron-up" />
+      <button className="fixed bottom-8 right-8 border-0 w-16 h-16 rounded-full drop-shadow-md bg-teal-400 text-white text-3xl font-bold  hover:bg-teal-600">
+      <FontAwesomeIcon icon={faChevronUp} className="h-4" />
       </button>
     </div>
   );
